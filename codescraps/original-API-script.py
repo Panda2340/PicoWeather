6beb3b82a6ed848423e9f41c6f1d055d6efa9404
Panda2340 <167357848+Panda2340@users.noreply.github.com>
2024-09-21 19:28:01 -0400

    Create original-API-script.py

---
+++ codescraps/original-API-script.py
@@ -0,0 +1,165 @@
+import openmeteo_requests
+
+import requests_cache
+import pandas as pd
+from retry_requests import retry
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+	"latitude": 52.52,
+	"longitude": 13.41,
+	"current": ["temperature_2m", "relative_humidity_2m", "apparent_temperature", "is_day", "precipitation", "rain", "showers", "snowfall", "cloud_cover", "wind_speed_10m", "wind_direction_10m", "wind_gusts_10m"],
+	"hourly": ["temperature_2m", "relative_humidity_2m", "dew_point_2m", "apparent_temperature", "precipitation_probability", "precipitation", "rain", "showers", "snowfall", "snow_depth", "cloud_cover", "cloud_cover_low", "cloud_cover_mid", "cloud_cover_high", "visibility", "wind_speed_10m", "wind_speed_80m", "wind_speed_120m", "wind_speed_180m", "wind_direction_10m", "wind_direction_80m", "wind_direction_120m", "wind_direction_180m", "wind_gusts_10m", "temperature_80m", "temperature_120m", "temperature_180m"],
+	"daily": ["sunrise", "sunset", "daylight_duration", "sunshine_duration", "uv_index_max", "precipitation_sum", "rain_sum", "showers_sum", "snowfall_sum", "precipitation_hours", "precipitation_probability_max", "wind_speed_10m_max", "wind_gusts_10m_max"],
+	"timezone": "America/New_York"
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation {response.Elevation()} m asl")
+print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+# Current values. The order of variables needs to be the same as requested.
+current = response.Current()
+current_temperature_2m = current.Variables(0).Value()
+current_relative_humidity_2m = current.Variables(1).Value()
+current_apparent_temperature = current.Variables(2).Value()
+current_is_day = current.Variables(3).Value()
+current_precipitation = current.Variables(4).Value()
+current_rain = current.Variables(5).Value()
+current_showers = current.Variables(6).Value()
+current_snowfall = current.Variables(7).Value()
+current_cloud_cover = current.Variables(8).Value()
+current_wind_speed_10m = current.Variables(9).Value()
+current_wind_direction_10m = current.Variables(10).Value()
+current_wind_gusts_10m = current.Variables(11).Value()
+
+print(f"Current time {current.Time()}")
+print(f"Current temperature_2m {current_temperature_2m}")
+print(f"Current relative_humidity_2m {current_relative_humidity_2m}")
+print(f"Current apparent_temperature {current_apparent_temperature}")
+print(f"Current is_day {current_is_day}")
+print(f"Current precipitation {current_precipitation}")
+print(f"Current rain {current_rain}")
+print(f"Current showers {current_showers}")
+print(f"Current snowfall {current_snowfall}")
+print(f"Current cloud_cover {current_cloud_cover}")
+print(f"Current wind_speed_10m {current_wind_speed_10m}")
+print(f"Current wind_direction_10m {current_wind_direction_10m}")
+print(f"Current wind_gusts_10m {current_wind_gusts_10m}")
+
+# Process hourly data. The order of variables needs to be the same as requested.
+hourly = response.Hourly()
+hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
+hourly_dew_point_2m = hourly.Variables(2).ValuesAsNumpy()
+hourly_apparent_temperature = hourly.Variables(3).ValuesAsNumpy()
+hourly_precipitation_probability = hourly.Variables(4).ValuesAsNumpy()
+hourly_precipitation = hourly.Variables(5).ValuesAsNumpy()
+hourly_rain = hourly.Variables(6).ValuesAsNumpy()
+hourly_showers = hourly.Variables(7).ValuesAsNumpy()
+hourly_snowfall = hourly.Variables(8).ValuesAsNumpy()
+hourly_snow_depth = hourly.Variables(9).ValuesAsNumpy()
+hourly_cloud_cover = hourly.Variables(10).ValuesAsNumpy()
+hourly_cloud_cover_low = hourly.Variables(11).ValuesAsNumpy()
+hourly_cloud_cover_mid = hourly.Variables(12).ValuesAsNumpy()
+hourly_cloud_cover_high = hourly.Variables(13).ValuesAsNumpy()
+hourly_visibility = hourly.Variables(14).ValuesAsNumpy()
+hourly_wind_speed_10m = hourly.Variables(15).ValuesAsNumpy()
+hourly_wind_speed_80m = hourly.Variables(16).ValuesAsNumpy()
+hourly_wind_speed_120m = hourly.Variables(17).ValuesAsNumpy()
+hourly_wind_speed_180m = hourly.Variables(18).ValuesAsNumpy()
+hourly_wind_direction_10m = hourly.Variables(19).ValuesAsNumpy()
+hourly_wind_direction_80m = hourly.Variables(20).ValuesAsNumpy()
+hourly_wind_direction_120m = hourly.Variables(21).ValuesAsNumpy()
+hourly_wind_direction_180m = hourly.Variables(22).ValuesAsNumpy()
+hourly_wind_gusts_10m = hourly.Variables(23).ValuesAsNumpy()
+hourly_temperature_80m = hourly.Variables(24).ValuesAsNumpy()
+hourly_temperature_120m = hourly.Variables(25).ValuesAsNumpy()
+hourly_temperature_180m = hourly.Variables(26).ValuesAsNumpy()
+
+hourly_data = {"date": pd.date_range(
+	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = hourly.Interval()),
+	inclusive = "left"
+)}
+hourly_data["temperature_2m"] = hourly_temperature_2m
+hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
+hourly_data["dew_point_2m"] = hourly_dew_point_2m
+hourly_data["apparent_temperature"] = hourly_apparent_temperature
+hourly_data["precipitation_probability"] = hourly_precipitation_probability
+hourly_data["precipitation"] = hourly_precipitation
+hourly_data["rain"] = hourly_rain
+hourly_data["showers"] = hourly_showers
+hourly_data["snowfall"] = hourly_snowfall
+hourly_data["snow_depth"] = hourly_snow_depth
+hourly_data["cloud_cover"] = hourly_cloud_cover
+hourly_data["cloud_cover_low"] = hourly_cloud_cover_low
+hourly_data["cloud_cover_mid"] = hourly_cloud_cover_mid
+hourly_data["cloud_cover_high"] = hourly_cloud_cover_high
+hourly_data["visibility"] = hourly_visibility
+hourly_data["wind_speed_10m"] = hourly_wind_speed_10m
+hourly_data["wind_speed_80m"] = hourly_wind_speed_80m
+hourly_data["wind_speed_120m"] = hourly_wind_speed_120m
+hourly_data["wind_speed_180m"] = hourly_wind_speed_180m
+hourly_data["wind_direction_10m"] = hourly_wind_direction_10m
+hourly_data["wind_direction_80m"] = hourly_wind_direction_80m
+hourly_data["wind_direction_120m"] = hourly_wind_direction_120m
+hourly_data["wind_direction_180m"] = hourly_wind_direction_180m
+hourly_data["wind_gusts_10m"] = hourly_wind_gusts_10m
+hourly_data["temperature_80m"] = hourly_temperature_80m
+hourly_data["temperature_120m"] = hourly_temperature_120m
+hourly_data["temperature_180m"] = hourly_temperature_180m
+
+hourly_dataframe = pd.DataFrame(data = hourly_data)
+print(hourly_dataframe)
+
+# Process daily data. The order of variables needs to be the same as requested.
+daily = response.Daily()
+daily_sunrise = daily.Variables(0).ValuesAsNumpy()
+daily_sunset = daily.Variables(1).ValuesAsNumpy()
+daily_daylight_duration = daily.Variables(2).ValuesAsNumpy()
+daily_sunshine_duration = daily.Variables(3).ValuesAsNumpy()
+daily_uv_index_max = daily.Variables(4).ValuesAsNumpy()
+daily_precipitation_sum = daily.Variables(5).ValuesAsNumpy()
+daily_rain_sum = daily.Variables(6).ValuesAsNumpy()
+daily_showers_sum = daily.Variables(7).ValuesAsNumpy()
+daily_snowfall_sum = daily.Variables(8).ValuesAsNumpy()
+daily_precipitation_hours = daily.Variables(9).ValuesAsNumpy()
+daily_precipitation_probability_max = daily.Variables(10).ValuesAsNumpy()
+daily_wind_speed_10m_max = daily.Variables(11).ValuesAsNumpy()
+daily_wind_gusts_10m_max = daily.Variables(12).ValuesAsNumpy()
+
+daily_data = {"date": pd.date_range(
+	start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = daily.Interval()),
+	inclusive = "left"
+)}
+daily_data["sunrise"] = daily_sunrise
+daily_data["sunset"] = daily_sunset
+daily_data["daylight_duration"] = daily_daylight_duration
+daily_data["sunshine_duration"] = daily_sunshine_duration
+daily_data["uv_index_max"] = daily_uv_index_max
+daily_data["precipitation_sum"] = daily_precipitation_sum
+daily_data["rain_sum"] = daily_rain_sum
+daily_data["showers_sum"] = daily_showers_sum
+daily_data["snowfall_sum"] = daily_snowfall_sum
+daily_data["precipitation_hours"] = daily_precipitation_hours
+daily_data["precipitation_probability_max"] = daily_precipitation_probability_max
+daily_data["wind_speed_10m_max"] = daily_wind_speed_10m_max
+daily_data["wind_gusts_10m_max"] = daily_wind_gusts_10m_max
+
+daily_dataframe = pd.DataFrame(data = daily_data)
+print(daily_dataframe)
