dadd8594b5c3e9f5c07909e546f58c5cec3f0dbc
Panda2340 <167357848+Panda2340@users.noreply.github.com>
2024-09-21 20:06:18 -0400

    Create import-c2.py

---
+++ codescraps/import-c2.py
@@ -0,0 +1,9 @@
+import network
+import urequests
+import time
+from pimoroni import Unicorn
+import openmeteo_requests
+import requests_cache
+import pandas as pd
+from retry_requests import retry
+#Alternativees for requests_cache and pandas will be required.
