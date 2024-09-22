0e6a435a2109da105b66517fa9958a1757a79bc5
Panda2340 <167357848+Panda2340@users.noreply.github.com>
2024-09-22 16:36:57 -0400

    Delete codescraps/configuration.py

--- codescraps/configuration.py
+++
@@ -1,2 +0,0 @@
-#This is just an idea, it may or may not be implemented
-#This script lets you assign different locations to different settings (changing what location it shows the weather for using the system in location-switch.py)
