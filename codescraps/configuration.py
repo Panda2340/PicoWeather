4b8a5d178e417129aaec04d6fafa288b135eb86d
Panda2340 <167357848+Panda2340@users.noreply.github.com>
2024-09-22 16:19:01 -0400

    Create configuration.py

---
+++ codescraps/configuration.py
@@ -0,0 +1,2 @@
+#This is just an idea, it may or may not be implemented
+#This script lets you assign different locations to different settings (changing what location it shows the weather for using the system in location-switch.py)
